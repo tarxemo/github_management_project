63e7818db9c56a620d0b71ac9922201646f0f42d
tarxemo <tarxemo@gmail.com>
2025-06-21 17:18:16 +0300

    database changes

--- poultry/settings.py
+++ poultry/settings.py
@@ -114,23 +114,23 @@ WSGI_APPLICATION = 'poultry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'leo',
-#         'USER': 'tarxemo',
-#         'PASSWORD': '@SuperCoder',
-#         'HOST': 'localhost',  # or your remote server IP
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'leo',
+        'USER': 'tarxemo',
+        'PASSWORD': '@SuperCoder',
+        'HOST': 'localhost',  # or your remote server IP
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

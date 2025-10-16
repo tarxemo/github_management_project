c0875e7c6595f1615b5045d77339dc75804bfd08
tarxemo <tarxemo@gmail.com>
2025-10-16 22:07:29 +0300

    statics

--- github_management_project/settings.py
+++ github_management_project/settings.py
@@ -304,7 +304,8 @@ USE_TZ = True
 # Static & Media Files
 # -----------------------------
 STATIC_URL = '/static/'
-STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+# STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.getenv("DJANGO_MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
